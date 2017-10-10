d8b41bec896d7ef9ecda09acd8475b26934c4974
Jason Baker <jason.th.baker@gmail.com>
2017-10-09 21:11:20 -0500

    V1

--- roster.js
+++ roster.js
@@ -1,18 +1,30 @@
 var teamMembers =
     {"Colin Bromley":
-        {photo:"<img-src=resources/colin.jpg",
-        text: "Colin is originally from Pennsylvania and has been racing bikes in Central Texas for 5 years"
-        }
+        {"photo":"<img src='resources/photos/colinbromley.jpg'>",
+        "text":"Colin is originally from Pennsylvania and has been racing bikes in Central Texas for 5 years Blah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah Blah"
+        },
      "Jason Baker":
-        {photo:"<img-src=resources/jason.jpg",
-        text: "Jason is from Austin and has been racing for 4 years."
+        {"photo":"<img src='resources/photos/jasonbaker.jpg'>",
+        "text":"Jason is from Austin and has been racing for 4 years. Blah Blah Blah Blah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah Blah"
         }
-    }
+    };
 
-function $(document).ready(function(){
-    $("#content").click(function(event){
-        var fullid = $(this).attr(id);
-        $("button").show(teamMembers.fullid.photo);
-        $("button").show(teamMembers.fullid.text);
+var cList = $('ul');
+
+$.each(teamMembers, function(key,value)){
+    var li = $('<li/>')
+        .addClass('ui-menu-item')
+        .attr('role', 'menuitem')
+        .innerHTML(key)
+        .appendTo(cList);
+};
+
+("#IDbuttons").innerHTML = cList;
+
+$(document).ready(function(){
+    $(".click").click(function(){
+		var fullid = $(this).text();
+        $("#roster-content").replaceWith('<div id="roster-content">'+teamMembers[fullid].photo+"<br>"+
+        teamMembers[fullid].text+"</div>")
     });
 });
