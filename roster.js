73d765e38968eae96f42404933fc9dd8046911bd
Jason Baker <jason.th.baker@gmail.com>
2017-10-09 23:08:24 -0500

    V1

--- roster.js
+++ roster.js
@@ -1,30 +1,22 @@
 var teamMembers =
     {"Colin Bromley":
         {"photo":"<img src='resources/photos/colinbromley.jpg'>",
-        "text":"Colin is originally from Pennsylvania and has been racing bikes in Central Texas for 5 years Blah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah Blah"
+        "text":"Colin is originally from Pennsylvania and has been racing bikes in Central Texas for 5 years"
         },
      "Jason Baker":
         {"photo":"<img src='resources/photos/jasonbaker.jpg'>",
-        "text":"Jason is from Austin and has been racing for 4 years. Blah Blah Blah Blah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah Blah"
-        }
+        "text":"Jason is from Austin and has been racing for 4 years."
+        },
+     "Michael Orsow":{"text":"Mike is a total baller from Oregon."},
     };
 
-var cList = $('ul');
-
-$.each(teamMembers, function(key,value)){
-    var li = $('<li/>')
-        .addClass('ui-menu-item')
-        .attr('role', 'menuitem')
-        .innerHTML(key)
-        .appendTo(cList);
-};
-
-("#IDbuttons").innerHTML = cList;
+$.each(teamMembers, function(key){
+    $("#NameButtons").append("<li>"+key+"</li>");
+    });
 
 $(document).ready(function(){
-    $(".click").click(function(){
-		var fullid = $(this).text();
-        $("#roster-content").replaceWith('<div id="roster-content">'+teamMembers[fullid].photo+"<br>"+
-        teamMembers[fullid].text+"</div>")
-    });
+    $("#NameButtons li").click(function(){
+      var fullid = $(this).text();
+      $("#roster-content").replaceWith('<div id="roster-content"><p>'+teamMembers[fullid].text+"</p></div>");
+      });
 });
